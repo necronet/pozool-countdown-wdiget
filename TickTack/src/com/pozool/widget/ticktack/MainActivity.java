3d064f561af88bd9d491fc2aa77d516b91fc29d5
Jose Ayerdis <jose.ayerdis@booking.com>
2013-05-27 04:32:49 +0200

    Create main activity

---
+++ TickTack/src/com/pozool/widget/ticktack/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pozool.widget.ticktack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
